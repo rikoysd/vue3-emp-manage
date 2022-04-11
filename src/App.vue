51c56748d8a699b29993a93d3fb9cdb4522440f2
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-11 09:53:11 +0900

    不要な画像削除

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
@@ -34,11 +32,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 a,
