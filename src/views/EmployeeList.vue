4e696de2339bbd35ca213c75d528850930e8aacc
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-11 13:38:56 +0900

    templateの定義づけ

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -1,9 +1,29 @@
 <template>
+  <div class="top-wrapper">
+    <div class="container">
+      <div>従業員数:10人</div>
+      <div class="row">
+        <table class="striped">
+          <thead>
+            <tr>
+              <th>名前</th>
+              <th>入社日</th>
+              <th>扶養人数</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="employeeDetail.html">山田太郎</a></td>
+              <td>2000/1/1</td>
+              <td>3人</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-
-<style scoped>
-</style>
+<style scoped></style>
