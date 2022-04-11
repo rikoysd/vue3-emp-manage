31fac2777f765eadb965916ffbd28ee4fa504699
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-11 10:49:09 +0900

    template, styleを定義

--- src/views/RegisterAdmin.vue
+++ src/views/RegisterAdmin.vue
@@ -1,10 +1,40 @@
 <template>
-<h1>管理者登録</h1>
+  <div class="name position">
+    <div>
+      <label for="lastName">姓</label>
+      <input type="text" id="lastName" />
+    </div>
+    <div>
+      <label for="firstName">名</label>
+      <input type="text" id="firstName" />
+    </div>
+  </div>
+  <div class="position">
+    <label for="mail">メールアドレス</label>
+    <input type="text" id="mail" />
+  </div>
+  <div class="position">
+    <label for="password">パスワード</label>
+    <input type="text" id="password" />
+  </div>
+  <div class="position">
+    <button type="button">登録</button>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
-
+<script setup lang="ts"></script>
 
 <style scoped>
+.name {
+  margin-top: 40px;
+}
+
+.position {
+  display: flex;
+  justify-content: center;
+}
+
+input {
+  margin-bottom: 20px;
+}
 </style>
